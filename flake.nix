d26d0e42b7aafcc744d49fa0cef3a3e57c025147
plsuwu <124419933+plsuwu@users.noreply.github.com>
2026-06-01 21:12:35 +1000

    initial commit

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs, ... }@inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        buildInputs = [
+          pkgs.emscripten
+          pkgs.bear
+        ];
+      };
+    };
+}
